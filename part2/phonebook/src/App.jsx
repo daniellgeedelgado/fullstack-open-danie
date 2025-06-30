549c97bc1b15d923adc93c1d69f798d36bdf23c6
Daniell Delgado <daniell.delgado3@gmail.com>
2025-06-30 19:09:58 +0800

    part 2 exercise 6 to 10

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-1234567' }
+  ])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const nameExists = persons.some(
+      (person) => person.name.toLowerCase() === newName.toLowerCase()
+    )
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    const newPerson = { name: newName, number: newNumber }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const personsToShow = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+
+      <h3>add a new</h3>
+      <PersonForm
+        onSubmit={addPerson}
+        nameValue={newName}
+        numberValue={newNumber}
+        onNameChange={handleNameChange}
+        onNumberChange={handleNumberChange}
+      />
+
+      <h3>Numbers</h3>
+      <Persons persons={personsToShow} />
+    </div>
+  )
+}
+
+export default App
---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,11 @@
+const Persons = ({ persons }) => (
+  <div>
+    {persons.map((person, index) => (
+      <p key={index}>
+        {person.name} {person.number}
+      </p>
+    ))}
+  </div>
+)
+
+export default Persons
